fc7ea0d782aa111e5e4c07dd81dd433375ab73a3
Yegor Knyazev <egorknyazev10@gmail.com>
2024-06-27 16:59:24 +0400

    some work

--- app.vue
+++ app.vue
@@ -1,6 +1,37 @@
+<script setup lang="ts">
+let dtl = useDefaultTimeline()
+
+let step = useState<number>('step', () => 1)
+let slides: number[] = [1, 2]
+
+function rerender() {
+	step.value++
+	if (step.value > (slides.at(-1) as number)) self.close()
+
+	let slideIndex = slides.findIndex(slideStep => slideStep === step.value)
+	if (!slideIndex) return
+	
+	let slideId = `slide-${slideIndex}`
+	let nextSlideId = `slide-${slideIndex + 1}`
+
+	dtl.to(slideId, { y: -20, opacity: 0 })
+	dtl.remove(slideId)
+	dtl.from(nextSlideId, { y: 20, opacity: 0 })
+}
+
+onMounted(() => {
+	document.addEventListener('mousedown', rerender)
+	document.addEventListener('keydown', rerender)
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div 
+		class="container cursor-pointer" 
+		ondragstart="return false;" 
+		ondrop="return false;"
+	>
+		<Intro id="slide-0" />
+		<Heading id="slide-1" />
+	</div>
 </template>
---
+++ components/heading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Component: heading
+  </div>
+</template>
+
+<style scoped></style>
